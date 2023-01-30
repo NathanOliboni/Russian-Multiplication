b893e7b0eaf5599374f68101768241095ed5b505
Nathan Oliboni <116748360+NathanOliboni@users.noreply.github.com>
2023-01-29 22:00:43 -0300

    Update Russian-multiplication.c
    Implementação de vetor para armazenar.

--- Russian-multiplication.c
+++ Russian-multiplication.c
@@ -13,29 +13,29 @@ int main(){
                 printf("\n[0] - FINALIZAR O PROGRAMA.\n[1] - MULTIPLICAR OS NUMEROS.\n");
                 scanf("%i", &op);
             }
-        int num1, num2, num, num0, tam=0, guardaNum=0;
-        int A[tam+1];
-        int B[tam+1];
-        int multiplicacoes = 0;    
-
-        switch (op){
-            case 0:
-                break;
-            case 1:
-                printf("Informe dois numeros para serem multiplicados: ");
-                scanf("%i %i", &num1, &num2);
-                num = num1;
-                num0 = num2;
-                
-                while(num1 != 0){
-                    num1 /= 2;
-                    tam += 1;
-                }
-
+           
+        int num1, num2, num, num0, tam=0, soma=0;
+        if(op == 1){
+            printf("Informe dois numeros para serem multiplicados: ");
+            scanf("%i %i", &num1, &num2);
+            num = num1;
+            num0 = num2;
+        
+            while(num1 != 0){
+                num1 /= 2;
+                tam += 1;
+            }
+            int A[tam+1];
+            int B[tam+1];
+            int i = 0, multiplicacoes = 0, j=0;
+            int guardaNum[i];
                 while(multiplicacoes != tam){
                     printf("%d | %d ", num, num0);
                     if(num % 2 != 0){
-                        guardaNum += num0;
+                        soma += num0;
+                        guardaNum[i] = num0;
+                        i+= 1;
+                        j+= 1;
                         printf("| %d", num0);
                     } else {
                         printf("| --");
@@ -47,11 +47,10 @@ int main(){
                     A[multiplicacoes] = num;
                     B[multiplicacoes] = num0;
                 }
-
-                printf("\nA multiplicacao dos numeros eh: %i\n ", guardaNum);
-                break;
-            default:
-                break;
-        }
+                   for (i=0;i<j;i++){
+                        printf("\n\n%d |", guardaNum[i]);
+                    }
+                printf("\nA multiplicacao dos numeros eh: %i\n ", soma);
+        } 
     }while(op != 0);
 }
